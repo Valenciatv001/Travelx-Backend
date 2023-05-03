0b123db46910b9f9bfddd2484c5f60a87b301181
Valenciatv001 <67506940+Valenciatv001@users.noreply.github.com>
2023-05-03 05:08:59 +0100

    Initial commit

---
+++ travelx__backend/schemas/pin.js
@@ -0,0 +1,59 @@
+// schemas/pin.js
+export default {
+    name: 'pin',
+    title: 'Pin',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'about',
+        title: 'About',
+        type: 'string',
+      },
+      {
+        name: 'destination',
+        title: 'Destination',
+        type: 'url',
+      },
+      {
+        name: 'category',
+        title: 'Category',
+        type: 'string',
+      },
+      {
+        name: 'image',
+        title: 'Image',
+        type: 'image',
+        options: {
+          hotspot: true
+        }
+      },
+      {
+        name: 'userId',
+        title: 'UserID',
+        type: 'string',
+      },
+      {
+        name: 'postedBy',
+        title: 'PostedBy',
+        type: 'postedBy',
+      },
+      {
+        name: 'svae',
+        title: 'Save',
+        type: 'array',
+        of: [{ type: 'save'}]
+      },
+      {
+        name: 'comments',
+        title: 'Comments',
+        type: 'array',
+        of: [{ type: 'comment'}]
+      },
+
+    ]
+  }
